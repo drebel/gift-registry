2e6dfac693cf3eb3aae0d09c48971a356d402539
drew <drewbeland@gmail.com>
2023-04-01 13:29:51 -0700

    used populate method in mongoose to display username of claimedby

--- controllers/gifts.js
+++ controllers/gifts.js
@@ -3,7 +3,7 @@ const Gift = require("../models/Gift");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const gifts = await Gift.find({ user: req.user.id });
+      const gifts = await Gift.find({ user: req.user.id }).populate('claimedBy', 'userName');
       res.render("profile.ejs", { gifts: gifts, user: req.user });
     } catch (err) {
       console.log(err);
