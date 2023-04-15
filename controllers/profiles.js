62673341d180324c58810361c3b558b020874e33
drew <drewbeland@gmail.com>
2023-04-15 16:58:22 -0700

    added the add friend button in the view. also changed some of the logic in the controller to handle the new input type

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,11 +1,16 @@
+const FriendList = require("../models/FriendList");
 const Gift = require("../models/Gift");
+const User = require("../models/User");
 //const FreindList = require("../models/FriendList");
 
 module.exports = {
     getProfile: async (req, res) => {
         try{
             const gifts = await Gift.find({ user: req.params.id }).populate('claimedBy', 'userName')
-            res.render("profile.ejs", {gifts: gifts, user: req.user, profile: req.params.id})
+            const profile = await User.findOne({_id: req.params.id})
+            const myFriends = await FriendList.findOne({user: req.user.id})
+            const isFriend = myFriends.friends.includes(req.params.id)
+            res.render("profile.ejs", {gifts: gifts, user: req.user, profile: profile, isFriend: isFriend})
         }catch(err){
             console.log(err)
         }
--- routes/friend.js
+++ routes/friend.js
@@ -5,7 +5,7 @@ const friendsController = require("../controllers/friends");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
-router.put("/addFriend", friendsController.addFriend);
+router.put("/addFriend/:id", friendsController.addFriend);
 
 router.put("/deleteFriend/:id", friendsController.deleteFriend);
 
